93de9567a1c3a70f7ab9540ab6bee5a6a0a884e3
jake hanes <jhanesx@yahoo.com>
2019-07-25 13:51:04 -0700

    fixxed spotify bug and got spotify to call correctly

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.f879ba71415c4135ad7b5c657e5e6bb9,
-  secret: process.env.ceb1d99b1aae45d387771ab5755903c2
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -2,27 +2,36 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 const axios = require('axios');
 
-var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var keys = require("./keys");
+var spotify = new Spotify(keys.spotify);
 
 let varOne = process.argv[2];
 let varTwo = process.argv[3];
 
-// if (varOne === "spotify-this-song") {
-//     spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//         if (err) {
-//           return console.log('Error occurred: ' + err);
-//         }
-       
-//       console.log(data); 
-//       });
-// }
+if (varOne === "spotify-this-song") {
+    spotify.search({ type: 'track', query: varTwo }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        let artist = data.tracks.items[0].album.artists[0].name;
+        let album = data.tracks.items[0].album.name;
+        let song = data.tracks.items[0].name;
+        let songLink = data.tracks.items[0].external_urls;
 
-if (varOne === "movie-this"){
-    
-    axios.get("http://www.omdbapi.com/?t="+ varTwo + "&y=&plot=short&apikey=8fba184d").then(
-  function(response) {
-    console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMBD Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\nEnjoy Your Movie");
-  }
-);
+        console.log(artist);
+        console.log(album);
+        console.log(song);
+        console.log(songLink);
+
+        
+    });
+}
+
+if (varOne === "movie-this") {
+
+    axios.get("http://www.omdbapi.com/?t=" + varTwo + "&y=&plot=short&apikey=8fba184d").then(
+        function (response) {
+            console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMBD Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\nEnjoy Your Movie");
+        }
+    );
 }
